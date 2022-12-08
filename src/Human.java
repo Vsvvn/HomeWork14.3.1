fc340191eaf30256e225feb9ee11994947604e79
Vsvvn <Vladsun@yandex.ru>
2022-12-08 15:03:23 +0300

    Сделано дз.

---
+++ src/Human.java
@@ -0,0 +1,44 @@
+public class Human {
+    public int yearOfBirth;
+    public String name;
+    public String town;
+    public String jobTitle;
+
+
+    public Human() {
+         this("Информация не указана",0,"Информация не указана","Информация не указана");
+    }
+
+    public Human(String name, int yearOfBirth, String town, String jobTitle) {
+
+        if (name == null || name.isEmpty()) {
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+        }
+
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }
+
+        if (town == null) {
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+
+        if (jobTitle == null) {
+            this.jobTitle = "Информация не указана";
+        } else {
+            this.jobTitle = jobTitle;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(ась) в " + yearOfBirth + " году." +
+                "Я работаю на должности " + jobTitle + " Будем знакомы!";
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Human maksim = new Human( "", -1900,null,"бренд-менеджера.");
+//        maksim.name = "Максим";
+//        maksim.yearOfBirth = 1988;
+//        maksim.town = "Минск";
+//        maksim.jobTitle = "бренд-менеджера.";
+
+        System.out.println(maksim);
+
+
+        Human anya = new Human("Аня",1993,"Москва","методиста образовательных программ.");
+//        anya.name = "Аня";
+//        anya.yearOfBirth = 1993;
+//        anya.town = "Москва";
+//        anya.jobTitle = "методиста образовательных программ.";
+
+        System.out.println(anya);
+
+
+        Human katya = new Human("Катя",1992,"Калининград","продакт-менеджера.");
+//        katya.name = "Катя";
+//        katya.yearOfBirth = 1992;
+//        katya.town = "Калининград";
+//        katya.jobTitle = "продакт-менеджера.";
+
+        System.out.println(katya);
+
+
+        Human artem = new Human("Артем",1995,"Москва","директора по развитию бизнеса.");
+//        artem.name = "Артем";
+//        artem.yearOfBirth = 1995;
+//        artem.town = "Москва";
+//        artem.jobTitle = "директора по развитию бизнеса.";
+
+        System.out.println(artem);
     }
 }
